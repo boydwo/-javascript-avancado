a96dddf1da3ef495ebfa259c0de40da574401726
Marcos Túlio Rodrigues <marcostulio06@hotmail.com>
2020-06-01 08:25:40 -0300

    finalizando Proxy

--- aluraframe/client/js/app/service/ProxyFactory.js
+++ aluraframe/client/js/app/service/ProxyFactory.js
@@ -3,7 +3,7 @@ class ProxyFactory {
   static create(objeto, props, acao) {
     return new Proxy(objeto, { // cria proxy
       get(target, prop, receiver) {    // target objeto (lista negocicaos), value que esta sendo passado no objeto
-        if (props.includes(prop) && typeof (target[prop]) == typeof (Function)) { // verifica se a prop q foi chama é alguma daquela e  se é uma function
+        if (props.includes(prop) && ProxyFactory._ehFuncao(target[prop])) { // verifica se a prop q foi chama é alguma daquela e  se é uma function
           return function () { //substitui o metodo/função por essa função nova:
             console.log(`interceptando ${prop}`);
             Reflect.apply(target[prop], target, arguments); // faz a função receber os parametros dela.
@@ -11,8 +11,19 @@ class ProxyFactory {
           }
         }
         return Reflect.get(target, prop, receiver);
+      },
+
+      set(target, prop, value, receiver) {
+        if (props.includes(prop)) {
+          acao(target);
+        }
+        return Reflect.set(target, prop, value, receiver);
+
+
       }
     });
   }
-
+  static _ehFuncao(func) {
+    return typeof (func) == typeof (Function);
+  }
 }
