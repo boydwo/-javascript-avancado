24ede405371ad9d96254f9d259169c3eac4a13eb
Marcos Túlio Rodrigues <marcostulio06@hotmail.com>
2020-06-04 19:18:05 -0300

    resolvendo Bug

--- aluraframe/client/js/app/service/ProxyFactory.js
+++ aluraframe/client/js/app/service/ProxyFactory.js
@@ -6,18 +6,20 @@ class ProxyFactory {
         if (props.includes(prop) && ProxyFactory._ehFuncao(target[prop])) { // verifica se a prop q foi chama é alguma daquela e  se é uma function
           return function () { //substitui o metodo/função por essa função nova:
             console.log(`interceptando ${prop}`);
-            Reflect.apply(target[prop], target, arguments); // faz a função receber os parametros dela.
-            return acao(target); //self._negociacoesView.update(target);
+            let retorno = Reflect.apply(target[prop], target, arguments); // faz a função receber os parametros dela.
+            acao(target); //self._negociacoesView.update(target);
+            return retorno;
           }
         }
         return Reflect.get(target, prop, receiver);
       },
 
       set(target, prop, value, receiver) {
+        let retorno = Reflect.set(target, prop, value, receiver);;
         if (props.includes(prop)) {
           acao(target);
         }
-        return Reflect.set(target, prop, value, receiver);
+        return retorno;
 
 
       }
--- aluraframe/client/js/app/views/View.js
+++ aluraframe/client/js/app/views/View.js
@@ -3,6 +3,11 @@ class View {
     this._elemento = elemento;
   }
 
+  template() {
+
+    throw new Error('O método template deve ser implementado');
+  }
+
   update(model) {
     this._elemento.innerHTML = this.template(model);
   }
